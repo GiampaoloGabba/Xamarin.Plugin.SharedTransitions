15eab9ce9a3421d0852422e5dd80ac7c8d59c56d
Evolutionlab <info@evolutionlab.it>
2019-08-08 01:01:52 +0200

    skipe aspect calculations when image is not AspectFit

--- src/SharedTransitions/Platforms/iOS/ViewExtensions.cs
+++ src/SharedTransitions/Platforms/iOS/ViewExtensions.cs
@@ -8,36 +8,42 @@ namespace Plugin.SharedTransitions.Platforms.iOS
     {
         internal static CGRect GetImageFrame(this UIImageView imageView)
         {
-            nfloat imageAspect = imageView.Image.Size.Width / imageView.Image.Size.Height;
-            nfloat boundslAspect = imageView.Frame.Size.Width / imageView.Frame.Size.Height;
-
-            if (imageAspect != boundslAspect && imageView.ContentMode == UIViewContentMode.ScaleAspectFit)
+            if (imageView.ContentMode == UIViewContentMode.ScaleAspectFit)
             {
-                //calculate new dimensions based on aspect ratio
-                nfloat newWidth   = imageView.Frame.Width * imageAspect;
-                nfloat newHeight  = newWidth / imageAspect;
-                nfloat marginTop  = 0;
-                nfloat marginLeft = 0;
+                nfloat imageAspect   = imageView.Image.Size.Width / imageView.Image.Size.Height;
+                nfloat boundslAspect = imageView.Frame.Size.Width / imageView.Frame.Size.Height;
 
-                if (newWidth > imageView.Frame.Width || newHeight > imageView.Frame.Height)
+                if (imageAspect != boundslAspect)
                 {
-                    //depending on which of the two exceeds the box dimensions set it as the box dimension
-                    //and calculate the other one based on the aspect ratio
-                    if (newWidth > newHeight)
+                    //calculate new dimensions based on aspect ratio
+                    nfloat newWidth   = imageView.Frame.Width * imageAspect;
+                    nfloat newHeight  = newWidth / imageAspect;
+                    nfloat marginTop  = 0;
+                    nfloat marginLeft = 0;
+
+                    if (newWidth > imageView.Frame.Width || newHeight > imageView.Frame.Height)
                     {
-                        newWidth  = imageView.Frame.Width;
-                        newHeight = newWidth / imageAspect;
-                        marginTop = (imageView.Frame.Height - newHeight) / 2;
+                        //depending on which of the two exceeds the box dimensions set it as the box dimension
+                        //and calculate the other one based on the aspect ratio
+                        if (newWidth > newHeight)
+                        {
+                            newWidth  = imageView.Frame.Width;
+                            newHeight = newWidth / imageAspect;
+                            marginTop = (imageView.Frame.Height - newHeight) / 2;
+                        }
+                        else
+                        {
+                            newHeight  = imageView.Frame.Height;
+                            newWidth   = (int) (newHeight * imageAspect);
+                            marginLeft = (imageView.Frame.Width - newWidth) / 2;
+                        }
                     }
-                    else
-                    {
-                        newHeight  = imageView.Frame.Height;
-                        newWidth   = (int)(newHeight * imageAspect);
-                        marginLeft = (imageView.Frame.Width - newWidth) / 2;
-                    }
+
+                    return new CGRect(imageView.Frame.X + marginLeft, imageView.Frame.Y + marginTop, newWidth,
+                        newHeight);
                 }
-                return new CGRect(imageView.Frame.X + marginLeft, imageView.Frame.Y + marginTop, newWidth, newHeight);
             }
+
             return new CGRect(imageView.Frame.X, imageView.Frame.Y, imageView.Frame.Width, imageView.Frame.Height);
         }
 
