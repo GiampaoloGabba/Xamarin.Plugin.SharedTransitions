6f6ca2f7f8fe150ec119b74bdffc6710b567da7d
Evolutionlab <info@evolutionlab.it>
2019-08-19 15:30:00 +0200

    bump xforms ver, create sample project for shell. Reorganize samples

---
+++ src/Sample/TransitionShellApp/TransitionShellApp/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using TransitionShellApp.Services;
+using TransitionShellApp.Views;
+
+namespace TransitionShellApp
+{
+	public partial class App : Application
+	{
+
+		public App()
+		{
+			InitializeComponent();
+
+			DependencyService.Register<MockDataStore>();
+			MainPage = new AppShell();
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ src/Sample/TransitionShellApp/TransitionShellApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TransitionShellApp.Models;
+
+namespace TransitionShellApp.ViewModels
+{
+	public class ItemDetailViewModel : BaseViewModel
+	{
+		public Item Item { get; set; }
+		public ItemDetailViewModel(Item item = null)
+		{
+			Title = item?.Text;
+			Item = item;
+		}
+	}
+}
