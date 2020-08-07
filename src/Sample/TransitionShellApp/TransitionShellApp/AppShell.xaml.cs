8877e865156c0e71278fd17598bba7949ed413c1
Evolutionlab <info@evolutionlab.it>
2020-08-07 20:02:58 +0200

    update shell demo to include transition events

--- src/Sample/TransitionShellApp/TransitionShellApp/AppShell.xaml.cs
+++ src/Sample/TransitionShellApp/TransitionShellApp/AppShell.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Plugin.SharedTransitions;
+﻿using System;
+using System.Diagnostics;
+using Plugin.SharedTransitions;
 
 namespace TransitionShellApp
 {
@@ -7,6 +9,21 @@ namespace TransitionShellApp
 		public AppShell()
 		{
 			InitializeComponent();
+		}
+
+		private void AppShell_OnTransitionStarted(object sender, EventArgs e)
+		{
+			Debug.WriteLine("Transition started");
+		}
+
+		private void AppShell_OnTransitionEnded(object sender, EventArgs e)
+		{
+			Debug.WriteLine("Transition ended");
+		}
+
+		private void AppShell_OnTransitionCancelled(object sender, EventArgs e)
+		{
+			Debug.WriteLine("Transition ended");
 		}
 	}
 }
